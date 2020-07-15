7faab54edbf95342bc8e8b9f4c7e9f70c64b1632
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-07-15 17:35:53 -0300

    api

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import logo from './trivia.png';
 import './App.css';
 
+import { getQuestions } from './services/api';
+
+console.log(getQuestions(localStorage.token))
+
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          SUA VEZ
-        </p>
+        <p>SUA VEZ</p>
       </header>
     </div>
   );
