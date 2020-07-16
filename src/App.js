c85eb6b6a2e1b88510133ddaefa741693b931496
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-07-16 18:47:39 -0300

    questions

--- src/App.js
+++ src/App.js
@@ -1,22 +1,8 @@
 import React from 'react';
-
-// import logo from './trivia.png';
+import Routes from './routes';
 import './App.css';
 
-<<<<<<< HEAD
-import { fetchApiToken } from './services/api';
-import Routes from './routes';
-
-function App(props) {
-  React.useEffect(() => {
-    fetchApiToken().then(() => props.getActionsQuestions(localStorage.token));
-  }, []);
-
-=======
-import Routes from './routes';
-
 function App() {
->>>>>>> d0fac180e74f293afcb9ae89185afa399f141a2b
   return (
     <div className="App">
       <Routes />
