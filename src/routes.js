731c059dd4dc8e75e9e0df39de7cc875c7bcf5b2
deboracosilveira <deboracosilveira@gmail.com>
2020-07-16 15:39:48 -0300

    question-page ok

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import { Home /* , Question  */ } from './pages';
+import { Home, Question } from './pages';
 
 const Routes = () => (
   <BrowserRouter>
     <Switch>
-      <Route path="/" component={Home} />
-      {/* <Route path="/question" component={Question} /> */}
+      <Route exact path="/" component={Home} />
+      <Route path="/question" component={Question} />
     </Switch>
   </BrowserRouter>
 );
