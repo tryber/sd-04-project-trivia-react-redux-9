ca77ad9c93d89dbc002f0155dd0df290b6d78b66
TerezaCarvalho <Familia@MacBook-Air-de-FamiliaCarvalhoAugusto.local>
2020-07-19 17:18:18 -0300

    localstorage header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,29 +1,23 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import './Header.css';
 
+const { name, score, gravatarEmail } = JSON.parse(localStorage.state.player);
 
-const Header = ({ name, score, gravatarMail }) => (
+const Header = () => (
   <div className="header">
-    <p data-testid="header-player-name">Player: {name}</p>
+    <p data-testid="header-player-name">Player:{name}</p>
     <p data-testid="header-score">Score:{score}</p>
     <img
       className="header-profile-picture"
       data-testid="header-profile-picture"
       alt={name}
-      src={gravatarMail}
+      src={gravatarEmail}
     />
   </div>
 );
 
-const mapStateToProps = (state) => ({
-  name: state.loginReducer.name,
-  score: state.loginReducer.score,
-  gravatarMail: state.loginReducer.gravatarMail,
-});
-
-export default connect(mapStateToProps, null)(Header);
+export default Header;
 
 Header.propTypes = {
   name: PropTypes.string.isRequired,
